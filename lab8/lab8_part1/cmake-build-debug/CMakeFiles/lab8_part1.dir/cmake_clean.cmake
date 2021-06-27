d5818a1de9c0c82f74440a32525c00c1ec699261
elena-mayorova <85339045+elena-mayorova@users.noreply.github.com>
2021-06-28 00:16:22 +0300

    Add files via upload

---
+++ lab8/lab8_part1/cmake-build-debug/CMakeFiles/lab8_part1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab8_part1.dir/main.cpp.o"
+  "lab8_part1.exe"
+  "lab8_part1.pdb"
+  "liblab8_part1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab8_part1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
